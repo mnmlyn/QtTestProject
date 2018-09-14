0fd7c4fc9e16f309f564a7e2dde8bc7323a3f5cc
mnmlyn <mnmlyn@163.com>
2018-09-14 20:42:37 +0800

    创建窗口的方法

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "logindlg.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
-
-    return a.exec();
+    LoginDlg dlg;
+    if(dlg.exec() == QDialog::Accepted)
+    {
+        w.show();
+        return a.exec();
+    }
+    else return 0;
 }
